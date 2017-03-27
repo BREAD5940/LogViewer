6dcd2448f3cb7c15696ad90ab05c6ce6d63a5c40
David Boles <david.e.boles22@gmail.com>
2017-03-27 09:25:24 -0700

    Created DoublePoint class

---
+++ src/org/team5940/log_viewer/display/DoublePoint.java
@@ -0,0 +1,13 @@
+package org.team5940.log_viewer.display;
+
+public class DoublePoint {
+
+	public final double x;
+	public final double y;
+	
+	public DoublePoint(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+}
